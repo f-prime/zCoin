bc9932f0bf11462c892e543107344bf5b6ecf6df
Max00355 <max00355@gmail.com>
2013-11-18 16:22:36 -0500

    fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 relay = 0 #1 is on 0 is off If 1 you must portforward port 6565
-brokers = [{0:"108.53.139.221", 1:6564}, {0:"108.53.139.221", 1:6565}]
+brokers = [{0:"108.53.139.221", 1:6564}]
 port = 6565
 host = "0.0.0.0"
 version = "0.0.2"
