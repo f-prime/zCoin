e28e820b321c392442d09d763d61346840329e21
root <max00355@gmail.com>
2014-04-02 18:01:18 -0400

    Took out a relay node

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import landerdb
 
 relay = 0
-brokers = [{"ip":"zcoin.zapto.org", "port":6564}, {"ip":"192.111.130.31", "port":6565}]
+brokers = [{"ip":"zcoin.zapto.org", "port":6564}]
 version = "0.2.1"
 host = "0.0.0.0"
 port = 6565
